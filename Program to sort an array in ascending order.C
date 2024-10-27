fdb9844e56d00b8f8a749793c62c5a4aa96fb34b
MRuhan17 <ruhanmulla07@gmail.com>
2024-10-27 15:54:17 +0530

    Create Program to sort an array in ascending order.C

---
+++ Program to sort an array in ascending order.C
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+
+    // Prompt the user to enter the number of elements
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int array[n]; // Declare an array of size n
+
+    // Input the elements of the array
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &array[i]);
+    }
+
+    // Sort the array using Bubble Sort
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                // Swap the elements
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    // Display the sorted array
+    printf("Sorted array in ascending order:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
